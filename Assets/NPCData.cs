8334131d5a187ec4c52d8c71d1eeb8d30f09061e
SquishyCode <mjmergal13@gmail.com>
2024-09-12 08:16:05 -0400

    IT WORKS
    the talking system + lil animation works. Hudson and Boris sprites are added

---
+++ Assets/NPCData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCData : MonoBehaviour
+{
+    
+    public string name;
+    public int timesTalkedTo;
+    public DialogueObject[] YapperList;
+
+    public float moveDist = 2f;
+    public float moveDuration = 1f; 
+
+    
+
+
+}
