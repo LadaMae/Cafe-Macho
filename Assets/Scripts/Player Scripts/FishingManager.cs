414f8d24fdcba74ce0f9c9d61bd468372fd55191
SquishyCode <mjmergal13@gmail.com>
2024-09-26 09:05:14 -0400

    Prototyped Fishing Game added

--- Assets/Scripts/Player Scripts/FishingManager.cs
+++ Assets/Scripts/Player Scripts/FishingManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,8 +18,20 @@ public class FishingManager : MonoBehaviour
     private Dictionary<Sprite, bool> gottenFish = new Dictionary<Sprite, bool>();
     public Dictionary<Sprite, GameObject> collectedFishImages = new Dictionary<Sprite, GameObject>();
 
+    //Fishing Minigame
+    public Image indicator; 
+    public GameObject targetZonePrefab;
+    public int numberOfTargetZones = 3;
+    public float minAngle = 0f; //Change to private later, keeping public for testing purposes.
+    public float maxAngle = 360f;
+    public float rotationSpeed = 200f;
+    private bool isFishing = true;
+    private float rotationAngle = 0f;
+    private List<Image> targetZones = new List<Image>(); 
     void Start()
     {
+        
+
         foreach (Sprite fsh in allFish)
         {
             GameObject newFish = Instantiate(fishImagePrefab.gameObject, allFishView.transform);
@@ -27,6 +41,104 @@ public class FishingManager : MonoBehaviour
             gottenFish.Add(fsh, false);
         }
     }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.R)){
+            GenerateTargetZones();
+        }
+        
+        if(isFishing)
+        {
+            rotationAngle += rotationSpeed * Time.deltaTime;
+            indicator.rectTransform.rotation = Quaternion.Euler(0,0, rotationAngle);
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+               CheckIfSucessful();
+            }
+        }
+    }
+
+    void GenerateTargetZones()
+    {
+        //Ensure clear board.
+        foreach(var zone in targetZones)
+        {
+            Destroy(zone.gameObject);
+        }
+        targetZones.Clear();
+
+        //Generate the new zones
+        List<float> usedAngles = new List<float>();
+        for (int i = 0; i < numberOfTargetZones; i++)
+        {
+            float randomAngle = GenerateAngleWithoutOverlap(usedAngles);
+            CreateTargetZone(randomAngle);
+            usedAngles.Add(randomAngle);
+        }
+
+
+    }
+
+    float GenerateAngleWithoutOverlap(List<float> usedAngles)
+    {
+        float randomAngle = UnityEngine.Random.Range(minAngle, maxAngle);
+        float minAngleDiff = 30f; //can change and shouldn't break.
+
+        bool isOverlapping;
+        int maxAttempts = 30;
+        int attempts = 0;
+
+        do //do's so cool 
+        {
+            isOverlapping = false;
+            foreach (float usedAngle in usedAngles)
+            {
+                if(Mathf.Abs(Mathf.DeltaAngle(randomAngle, usedAngle)) < minAngleDiff)
+                {
+                    isOverlapping = true;
+                    break;
+                }
+            }
+
+            if(isOverlapping)
+            {
+                randomAngle = UnityEngine.Random.Range(minAngle, maxAngle);
+            }
+
+            attempts++;
+            if(attempts > maxAttempts)
+            {
+                Debug.Log("Max Attempts Reached to generate fishing target zones.");
+                break;
+            }
+        } while(isOverlapping);
+
+        return randomAngle;
+    }
+
+     void CreateTargetZone(float angle)
+    {
+        //Instantiate new zone
+        GameObject newTargetZone = Instantiate(targetZonePrefab, indicator.transform.parent);
+        Image targetZoneImage = newTargetZone.GetComponent<Image>();
+
+        //I hate math.
+        float radians = angle * Mathf.Deg2Rad;
+        float radius = indicator.rectTransform.rect.width / 2f; //Completely based on the indicator being a circle
+
+        float xPos = Mathf.Cos(radians) * radius;
+        float yPos = Mathf.Sin(radians) * radius;
+        
+        targetZoneImage.rectTransform.anchoredPosition = new Vector2(xPos,yPos);
+
+        //Correct angle and place it in perimiter of indicator object.
+        targetZoneImage.rectTransform.localRotation = Quaternion.Euler(0,0, angle);
+        targetZones.Add(targetZoneImage);
+    }
+
     public void generateFish()
     {
         gachaFish();
@@ -36,7 +148,7 @@ public class FishingManager : MonoBehaviour
 
     public void gachaFish()
     {
-        int rate = Random.Range(0, 100);
+        int rate = UnityEngine.Random.Range(0, 100);
 
         if (rate == 99)
         {
@@ -48,7 +160,7 @@ public class FishingManager : MonoBehaviour
         else if (rate >= 94)
         {
             //uncommon fish
-            Sprite fsh = allFish[Random.Range(1, 3)];
+            Sprite fsh = allFish[UnityEngine.Random.Range(1, 3)];
             fishImageUI.sprite = fsh;
             checkFish(fsh);
             Debug.Log("uncommon");
@@ -56,7 +168,7 @@ public class FishingManager : MonoBehaviour
         else
         {
             //common
-            Sprite fsh = allFish[Random.Range(3, allFish.Count)];
+            Sprite fsh = allFish[UnityEngine.Random.Range(3, allFish.Count)];
             fishImageUI.sprite = fsh;
             checkFish(fsh);
             Debug.Log("common");
@@ -80,4 +192,28 @@ public class FishingManager : MonoBehaviour
             collectedFishImages[currentFish].GetComponent<Image>().color = Color.white;
         }
     }
+
+    void CheckIfSucessful()
+    {
+        float angle = indicator.rectTransform.eulerAngles.z; //If Img type rectTransform is missing in between the .
+
+        foreach (var targetZone in targetZones)
+        {
+            //30f is arbitrary to set the size of the target zone.
+            float targetStart = targetZone.rectTransform.eulerAngles.z - 30f;
+            float targetEnd = targetZone.rectTransform.eulerAngles.z + 30f;
+
+            if(angle >= targetStart && angle <= targetEnd)
+            {
+                Debug.Log("Successful Catch!");
+                targetZone.color = Color.green; //For feedback
+                return;
+            }
+        }
+
+        //We might want to repeat the rotation at nauseum until player gets the fish as to avoid stress?
+        Debug.Log("Lost Fish");
+        //isFishing = false;
+    }
+
 }
